b3b804c9c01c91df659b85b5406326da2a552d32
fviel <nandoviel@hotmail.com>
2021-04-22 15:53:28 -0300

    corrigido o bug da imagem da splash que não estava reconhecendo a logo de bg

--- lib/splash/splash_page.dart
+++ lib/splash/splash_page.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:nlw_app/core/app_gradients.dart';
+import 'package:nlw_app/core/app_images.dart';
 
 class SplashPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: AppGradients.linear,
+        ),
+        child: Center(
+          //A Sacada é que defini todos os paths de imagem em strings nessas classes
+          child: Image(
+              image: AssetImage('assets/images/logo.png'),
+          ),
+        ),
+      ),
     );
   }
 }
