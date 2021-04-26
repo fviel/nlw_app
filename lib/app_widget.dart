14af24148963d6eb3f13bc14a61a490333fa935a
fviel <nandoviel@hotmail.com>
2021-04-25 23:01:34 -0300

    adicionado o componente de botão pela metade

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -7,8 +7,8 @@ class AppWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "DevQuiz",
-      home: HomePage(),
-      //home: ChallengePage(),
+      //home: HomePage(),
+      home: ChallengePage(),
     );
   }
 }
---
+++ lib/challenge/next_button_widget.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class NextButtonWidget extends StatelessWidget {
+
+  final String label;
+
+  NextButtonWidget({this.label});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+        onPressed: null,
+        child: Text(
+          label,
+          style: TextStyle(
+
+          )
+        ));
+
+  }
+}
