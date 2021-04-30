e11a731e768455d3250d39f560c3e074866f8e31
fviel <nandoviel@hotmail.com>
2021-04-30 18:19:41 -0300

    voltei a usar a splash

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,14 +1,16 @@
 import 'package:nlw_app/challenge/challenge_page.dart';
 import 'package:flutter/material.dart';
 import 'package:nlw_app/home/home_page.dart';
+import 'package:nlw_app/splash/splash_page.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "DevQuiz",
-      home: HomePage(),
+      //home: HomePage(),
       //home: ChallengePage(),
+      home: SplashPage(),
     );
   }
 }
--- lib/splash/splash_page.dart
+++ lib/splash/splash_page.dart
@@ -1,9 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:nlw_app/core/app_gradients.dart';
+import 'package:nlw_app/home/home_page.dart';
 
 class SplashPage extends StatelessWidget {
+
+
+  // SplashPage(){
+  //   //não posso fazer async e await no construtor, por isso tenho que usar o then
+  // }
+
   @override
   Widget build(BuildContext context) {
+
+    //usei o pushReplacement, pois num é pra empilhar tela, é pra substituir
+    Future.delayed(Duration(seconds: 1)).then(
+          (_) => Navigator.pushReplacement(context, MaterialPageRoute(
+        builder: (context) => HomePage(),
+      )),
+    );
+
     return Scaffold(
       body: Container(
         decoration: BoxDecoration(
