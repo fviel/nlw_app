86400ff0080abae24dcafb02775d40ec04a10526
fviel <nandoviel@hotmail.com>
2021-05-02 23:13:47 -0300

    detalhes de import

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -6,8 +6,6 @@ class AppWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "DevQuiz",
-      //home: HomePage(),
-      //home: ChallengePage(),
       home: SplashPage(),
     );
   }
--- lib/widgets/scorecard/score_card_widget.dart
+++ lib/widgets/scorecard/score_card_widget.dart
@@ -2,14 +2,11 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:nlw_app/core/app_colors.dart';
 import 'package:nlw_app/core/app_text_styles.dart';
-import 'package:nlw_app/widgets/chart/chart_widget.dart';
 import 'package:nlw_app/widgets/chart/chart_widget_animado.dart';
 
 class ScoreCardWidget extends StatelessWidget {
   final double value;
   ScoreCardWidget({@required this.value});
-
-
 
   @override
   Widget build(BuildContext context) {
